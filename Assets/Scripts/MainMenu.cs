5a257845a35d1bd55b57f19893b066df66593040
Mano Kiku <nunununununununununuunu@gmail.com>
2024-09-16 19:18:31 +0300

    Added DB script, sign in menu and changed ui

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject settings;
     public GameObject mainMenu;
-    public void ExitGame() {
+    public GameObject signInMenu;
+
+    public TMP_Text username, password, passwordConfirm, email;
+    public static void ExitGame() {
         Application.Quit();
     }
 
@@ -15,8 +23,28 @@ public class MainMenu : MonoBehaviour
         mainMenu.SetActive(false);
     }
 
-        public void HideSettings() {
+    public void HideSettings() {
         settings.SetActive(false);
         mainMenu.SetActive(true);
+    }
+
+    public void ShowSignIn() {
+        signInMenu.SetActive(true);
+        settings.SetActive(false);
+    }
+
+    public void HideSignIn() {
+        signInMenu.SetActive(false);
+        settings.SetActive(true);
+    }
+
+    public void SignIn() {
+        DataTable users = AppDataBase.GetTable("SELECT * FROM Users WHERE Username = '" + username.text + "'OR email = '" + email.text + "';");
+        if (users.Rows == null) {
+            Debug.Log("Not register");
+        }
+        else {
+            Debug.Log("Register");
+        }
     }
 }
